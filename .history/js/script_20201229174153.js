2ecfdc2f929dea3e708b581a12384c791d95419c
Your Name <you@example.com>
2020-12-31 01:25:04 +0100

    first commit

---
+++ .history/js/script_20201229174153.js
@@ -0,0 +1,21 @@
+var typed = new Typed("#typed",{
+    strings: ["Français","English","Español", "Português", "Italiano", "Română", "Svenska", "Deutsch", "עִבְרִית"],
+    typeSpeed: 100,
+    backSpeed:50,
+    loop: true
+});
+
+$(function(){
+    $(".navbar a, footer a").on("click", function(event){
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('body').animate({scrollTop: $(hash).offset().top} , 900, function(){window.location.hash =hash})
+    });
+
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+        }); 
+
+
+})
---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    $(".navbar a, footer a").on("click", function(event){
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('body').animate({scrollTop: $(hash).offset().top} , 900, function(){window.location.hash =hash})
+    });
+
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+        }); 
+
+    var typed = new Typed("#typed",{
+        strings: ["Français","English","Español", "Português", "Italiano", "Română", "Svenska", "Deutsch", "עִבְרִית"],
+        typeSpeed: 100,
+        backSpeed:50,
+        loop: true
+    });
+})
