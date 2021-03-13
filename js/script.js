14946808583708d2668ffd68f3c775bf5316957b
0Memo <67073007+0Memo@users.noreply.github.com>
2021-03-13 14:56:45 +0100

    Update script.js
    updates to the script.js file

--- js/script.js
+++ js/script.js
@@ -16,4 +16,33 @@ $(function(){
         backSpeed:50,
         loop: true
     });
+
+    $('#contact-form').submit(function(e) {
+
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+
+        $.ajax({
+            type:'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(result) {
+                if(result.isSuccess) {
+                    $("#contact-form").append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté &#x1f4ab; </p>");
+                    $("#contact-form")[0].reset();
+                } else {
+                    $('#name + .comments').html(result.nameError);
+                    $('#company + .comments').html(result.companyError);
+                    $('#email + .comments').html(result.emailError);
+                    $('#phone + .comments').html(result.phoneError);
+                    $('#message + .comments').html(result.messageError);
+                }
+
+            }
+
+        });
+
+    });
 })
