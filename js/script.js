f48ed5a49f5c531b7fdfaaf00d09ddc28c23211a
Guillermo <67073007+0Memo@users.noreply.github.com>
2021-09-19 12:16:02 +0200

    Update script.js
    file update

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ $(function(){
         var val = "";
         val = this.value;
 
-        if(/^(+\d+(\s|-))?0\d(\s|-)?(\d{2}(\s|-)?){4}$/.test(this.value)){
+        if(/^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/.test(this.value)){
             var tips4 = document.getElementById('tips4');
             this.style.border="2px solid green";
             this.style.color="black";
