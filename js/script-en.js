12b75d9f7cb7ca1b1083414b15e147c1dbc55f3f
Guillermo <67073007+0Memo@users.noreply.github.com>
2021-12-24 12:08:25 +0100

    Update script-en.js
    file update

--- js/script-en.js
+++ js/script-en.js
@@ -1,8 +1,3 @@
-// English language page access
-    document.getElementById("english").onclick = function () {
-        location.href = "english_version.html";
-    };
-
 // English contact-form version
     var tips31 = document.getElementById('tips31');
     var tips32 = document.getElementById('tips32');
